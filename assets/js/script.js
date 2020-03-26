fd0e415856bd63b4dc67827b0268369ab5594ba6
neeerajtk <neerajtk560@gmail.com>
2020-03-26 12:38:31 +0530

    basic functionalities added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
-$("li").click(function(){
+$("ul").on("click","li",function(){
     $(this).toggleClass("completed");
+});                     
+
+$("ul").on("click","span",function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+       // this here is not the same this above.. refers to li not span
+    });
+    // $(this).parent().remove();
+    event.stopPropagation();
 }); 
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13)
+    {
+      var todoText = $(this).val();
+      $(this).val("");
+      $("ul").append("<li><span>X </span>"+todoText+"</li>");
+      
+    }
+});
